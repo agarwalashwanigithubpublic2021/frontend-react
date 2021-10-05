2913ae0bd0dedfd9f0d1a86503c6030f6b46d8fe
agarwalashwani2020@gmail.com <agarwalashwani2020@gmail.com>
2021-10-05 20:46:57 +0530

    Css changes

--- src/pages/TariffsPage.tsx
+++ src/pages/TariffsPage.tsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/dist/styles/ag-grid.css";
-import "ag-grid-community/dist/styles/ag-theme-alpine-dark.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 import { RouteComponentProps, withRouter } from "react-router-dom";
 import { Tariffs } from "../models/Tariffs";
 import TerrifStore from "../stores/TariffStore";
-
 
 
 export interface ITariffsPageProps extends RouteComponentProps {
@@ -16,6 +15,7 @@ const TariffsPage: React.FC<ITariffsPageProps> = (props: ITariffsPageProps) => {
     const [terrifsData, setTerrifsData] = useState<Tariffs[]>([]);
     const [consumptionValue, setConsumptionValue] = useState<number>();
     const [allTerrifsAPIError, setAllTerrifsAPIError] = useState("");
+    const [spinner, setSpinner] = useState(false);
 
     const columnDefs1 = [
         { headerName: 'Tariff Name', field: 'tariffName', filter: "agTextColumnFilter", sortable: true },
@@ -39,36 +39,25 @@ const TariffsPage: React.FC<ITariffsPageProps> = (props: ITariffsPageProps) => {
         overlayNoRowsTemplate: allTerrifsAPIError.length > 0 ? '<span>No Data</span>' : ''
     };
 
-
-    //const test = [{ "tarrifName": "Packaged tariff", "annualCosts": 800 }, { "tarrifName": "basic electricity tariff", "annualCosts": 12060 }];
-
-    // const rowData = [
-    //     { make: 'Toyota', model: 'Celica', price: 35000 },
-    //     { make: 'Ford', model: 'Mondeo', price: 32000 },
-    //     { make: 'Porsche', model: 'Boxter', price: 72000 }
-    // ];   
-
     async function fetchAllTerrifs(consumption: number) {
+        setSpinner(true);
         await TerrifStore.getTariffsByAnnualConsumption(consumption);
         const { tariffsByAnnualConsumption: terrifs, error } = TerrifStore.data;
 
-        //console.log("ERROR-",error);
-
         if (error.length > 0) {
-            //console.log("ERROR-", error);
             setAllTerrifsAPIError(error);
             setTerrifsData([]);
         }
         else {
-            // console.log("terrifs--", terrifs);
-            // console.log("terrifs--", terrifs.data);
+
             setTerrifsData(terrifs.data);
             setAllTerrifsAPIError("");
         }
+        setSpinner(false);
     }
 
     const handleValidationForConsumption = (e) => {
-        //console.log("text-", e.target.value);
+
         setConsumptionValue(Number(e.target.value));
     }
 
@@ -83,30 +72,39 @@ const TariffsPage: React.FC<ITariffsPageProps> = (props: ITariffsPageProps) => {
     }
 
     return (
-        <div id="ag-grid" style={{ height: "100%", width: "100%" }} className="ag-theme-alpine-dark">
+        <>
 
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <input value={consumptionValue} type="number" onChange={handleValidationForConsumption} placeholder="Consumption (kWh/year)" />
-                        <div style={allTerrifsAPIError === "" ? { visibility: 'hidden' } : { visibility: 'visible', color: 'red' }}>{allTerrifsAPIError}</div>
+            <div id="ag-grid" style={{ height: '100%', width: '100%' }} className="ag-theme-alpine parent-container">
+
+                <div className="container">
+
+                    <div className="row parent-container">
+                        <div className="col" style={{ marginBottom: '20px', marginTop: '20px' }}>
+
+                            <input value={consumptionValue} className="input-consumptionKwh" type="number" onChange={handleValidationForConsumption} placeholder="Consumption (kWh/year)" />
+                            <div style={allTerrifsAPIError === "" ? { visibility: 'hidden' } : { visibility: 'visible', color: 'red' }}>{allTerrifsAPIError}</div>
+                        </div>
+                        <div className="col" style={{ marginBottom: '20px' }}>
+                            <button className="btn btn-success" onClick={fetchTariffComparer}>Load Tariff</button>
+                        </div>
+                        <div className="col" style={{ marginBottom: '10px' }}>
+                            {spinner === true ? <img src="/assets/images/loader.gif" width={150} height={100} alt="loader" /> : null}
+                        </div>
                     </div>
-                    <div className="col">
-                        <button onClick={fetchTariffComparer}>Compare</button>
-                    </div>
+
+
                 </div>
 
+                <div className="ag-theme-balham" style={{ height: '200px', width: '420px' }}>
+                    <AgGridReact
+                        gridOptions={gridOptions}
+                        columnDefs={columnDefs1}
+                        rowData={terrifsData}
+                    >
+                    </AgGridReact>
+                </div>
             </div>
-
-            <div className="ag-theme-balham" style={{ height: '500px', width: '1000px' }}>
-                <AgGridReact
-                    gridOptions={gridOptions}
-                    columnDefs={columnDefs1}
-                    rowData={terrifsData}
-                >
-                </AgGridReact>
-            </div>
-        </div>
+        </>
     );
 }
 
